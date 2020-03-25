79464ec7e3e3904edc6f7df5335a023a8ae2b814
afzal <afzalamansuri392@gmail.com>
2020-03-25 11:32:53 +0530

    analysis project-movie

---
+++ ActorMovies/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorMovies.Models
+{
+   public  class Actor
+    {
+        public int ActorId { get; set; }
+        public string ActorName { get; set; }
+        public string Citizeship { get; set; }
+
+    }
+}
